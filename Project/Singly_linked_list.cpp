43c99ab1cdb4c540b4595ceaf368b856559fdf82
MesnerH <hubertmissar04@gmail.com>
2025-04-12 21:19:45 +0200

    added methods declarations in singly linked list cpp

---
+++ Project/Singly_linked_list.cpp
@@ -0,0 +1,13 @@
+#include "Singly_linked_list.hpp"
+#include <iostream>
+
+Singly_linked_list::Singly_linked_list() {}					// constructor
+Singly_linked_list::~Singly_linked_list() {}				// desctuctor
+void Singly_linked_list::push_front(int value) {}			// adds an element at the front of the list
+void Singly_linked_list::push_back(int value) {}			// adds an element at the back of the list
+void Singly_linked_list::remove_front() {}					// removes an element from the front of the list
+void Singly_linked_list::remove_back() {}					// removes an element from the back of the list
+void Singly_linked_list::print_forward() {}					// prints all elements of the list from front to back
+bool Singly_linked_list::find(int value) {}					// searches for an element in the list
+void Singly_linked_list::remove_randomly() {}				// removes a random element from the list
+void Singly_linked_list::add_randomly(int value) {}			// adds an element at a random position
