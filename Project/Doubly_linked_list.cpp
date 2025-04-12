b51e0fb54c3d48ee4b68db722df16b29dfe1f1ab
MesnerH <hubertmissar04@gmail.com>
2025-04-12 23:31:27 +0200

    added rest of declarations for doubly linked list

--- Project/Doubly_linked_list.cpp
+++ Project/Doubly_linked_list.cpp
@@ -1,12 +1,12 @@
 #include "Doubly_linked_list.hpp"
 #include <iostream>
 
-Doubly_linked_list::Doubly_linked_list()			// constructor
+Doubly_linked_list::Doubly_linked_list()		// constructor
 {
 	head = nullptr;
 	tail = nullptr;
 }
-Doubly_linked_list::~Doubly_linked_list()			// desctuctor
+Doubly_linked_list::~Doubly_linked_list()		// desctuctor
 {
 	Node* current = head;
 	while (current)
@@ -16,7 +16,7 @@ Doubly_linked_list::~Doubly_linked_list()			// desctuctor
 		current = next;
 	}
 }
-void Doubly_linked_list::push_front(int value)		// adds an element at the front of the list
+void Doubly_linked_list::push_front(int value)	// adds an element at the front of the list 
 {
 	Node* new_Node = new Node(value);
 
@@ -32,7 +32,7 @@ void Doubly_linked_list::push_front(int value)		// adds an element at the front 
 	}
 	std::cout << "Added " << value << " at the front" << std::endl;
 }
-void Doubly_linked_list::push_back(int value)		// adds an element at the back of the list
+void Doubly_linked_list::push_back(int value)	// adds an element at the back of the list
 {
 	Node* new_Node = new Node(value);
 
@@ -48,7 +48,7 @@ void Doubly_linked_list::push_back(int value)		// adds an element at the back of
 	}
 	std::cout << "Added " << value << " at the back" << std::endl;
 }
-void Doubly_linked_list::remove_front()				// removes an element from the front of the list
+void Doubly_linked_list::remove_front()			// removes an element from the front of the list
 {
 	if (head == nullptr)
 	{
@@ -69,7 +69,7 @@ void Doubly_linked_list::remove_front()				// removes an element from the front 
 	}
 	std::cout << "Element removed from the front" << std::endl;
 }
-void Doubly_linked_list::remove_back()				// removes an element from the back of the list
+void Doubly_linked_list::remove_back()			// removes an element from the back of the list
 {
 	if (head == nullptr)
 	{
@@ -90,13 +90,124 @@ void Doubly_linked_list::remove_back()				// removes an element from the back of
 	}
 	std::cout << "Element removed from the back" << std::endl;
 }
-void Doubly_linked_list::print_forward()			// prints all elements of the list from front to back
-{ }
-void Doubly_linked_list::print_backward()			// prints all elements of the list from back to front
-{ }
-bool Doubly_linked_list::find(int value)			// searches for an element in the list
-{ }
-void Doubly_linked_list::remove_randomly()			// removes a random element from the list
-{ }
+void Doubly_linked_list::print_forward()		// prints all elements of the list from front to back
+{
+	Node* current = head;
+
+	while (current)
+	{
+		std::cout << current->data << " ";
+		current = current->next;
+	}
+	std::cout << std::endl;
+}
+void Doubly_linked_list::print_backward()		// prints all elements of the list from back to front
+{
+	Node* current = tail;
+
+	while (current)
+	{
+		std::cout << current->data << " ";
+		current = current->previous;
+	}
+	std::cout << std::endl;
+}
+bool Doubly_linked_list::find(int value)		// searches for an element in the list
+{
+	Node* current = head;
+
+	while (current)
+	{
+		if (current->data == value)
+		{
+			return true;
+		}
+		current = current->next;
+	}
+	return false;
+}
+void Doubly_linked_list::remove_randomly()		// removes a random element from the list
+{
+	if (head == nullptr)
+	{
+		std::cout << "List is empty - there is nothing to remove" << std::endl;
+		return;
+	}
+	Node* current = head;
+
+	int size = 0;
+	while (current)
+	{
+		size++;
+		current = current->next;
+	}
+
+	int random_index = std::rand() % size;
+	current = head;
+
+	for (int i = 0; i < random_index; i++)
+	{
+		current = current->next;
+	}
+
+	int removed_value = current->data;
+
+	if (current == head)
+	{
+		remove_front();
+	}
+	else if (current == tail)
+	{
+		remove_back();
+	}
+	else
+	{
+		current->previous->next = current->next;
+		current->next->previous = current->previous;
+		delete current;
+		std::cout << "Removed " << removed_value << " from index " << random_index << std::endl;
+	}
+}
 void Doubly_linked_list::add_randomly(int value)	// adds an element at a random position
-{ }
+{
+	if (head == nullptr)
+	{
+		push_front(value);
+		return;
+	}
+	Node* current = head;
+
+	int size = 0;
+	while (current)
+	{
+		size++;
+		current = current->next;
+	}
+
+	int random_index = std::rand() % (size + 1);
+	Node* new_node = new Node(value);
+	
+	if (random_index == 0)
+	{
+		push_front(value);
+	}
+	else if (random_index == size)
+	{
+		push_back(value);
+	}
+	else
+	{
+		current = head;
+		for (int i = 0; i < random_index - 1; i++)
+		{
+			current = current->next;
+		}
+
+		new_node->next = current->next;
+		new_node->previous = current;
+		current->next->previous = new_node;
+		current->next = new_node;
+
+		std::cout << "Added " << value << " at index " << random_index << std::endl;
+	}
+}
