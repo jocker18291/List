8f3848c301b51d37d584d22de2e1a765b3f2666f
root <root@DESKTOP-9415EC1.>
2025-04-14 20:44:03 +0200

    Implemented resize function

--- Project/arrayList.cpp
+++ Project/arrayList.cpp
@@ -9,3 +9,13 @@ ArrayList::ArrayList() {
 ArrayList::~ArrayList() {
     delete[] arr;
 }
+
+void ArrayList::resize() {
+    capacity *= 2;
+    int* newArr = new int[capacity];
+    for(int i = 0; i < size; ++i) {
+        newArr[i] = arr[i];
+    }
+    delete[] arr;
+    arr = newArr;
+}
